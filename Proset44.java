3b0bad41ebcf7324a439bc89a2e35e6344f6d2d4
Suriyarithik <suriyavedsum@gmail.com>
2017-07-14 14:31:11 +0530

    Create Proset44.java

---
+++ Proset44.java
@@ -0,0 +1,32 @@
+# Proset44
+import javax.swing.Spring;
+import java.util.Scanner;
+public class Proset44 
+{
+  public static void main(Spring[] args)
+  {
+  Sacnner s=new Scanner(System.in);
+  String ch=s.next();
+  String ch1=s.next();
+  char a[]=ch.toLowerCase.toCharArray();
+  char b[]=ch1.toLowercase.toCharArray();
+  if(a.length==b.length)
+  {
+  Arrays.sort(a);
+  arrays.sort(b);
+  if(Arrays.equals(a,b));
+  {
+  System.out.println("anagram");
+  }
+  else
+  {
+  System.out.println("not anagram");
+  }
+  }
+  else
+   {
+   System.out.println("length ineuality");
+  }
+  }
+  
+  }
